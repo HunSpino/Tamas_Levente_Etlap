eb9b46ea17a40c34113ebab940e85ba70b7add54
HunSpino <tamasleventelaszlo99@gmail.com>
2022-01-19 16:34:43 +0100

    listázási design kész

--- src/main/java/com/example/etlap/HelloController.java
+++ src/main/java/com/example/etlap/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.etlap;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,14 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onHozzadasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onModositasButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onTorlesButtonClick(ActionEvent actionEvent) {
     }
 }
